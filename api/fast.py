9a22fea7238ab114c5ef8e9414f34c82058c61ec
clemPera <clement.pera@gmail.com>
2026-03-12 16:13:03 +0100

    feat: base for api predict

--- api/fast.py
+++ api/fast.py
@@ -1,8 +1,17 @@
-import pandas as pd
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 
+import pandas as pd
+from PIL import Image
+
+from models.tabular import XGBoost
+from models.images.dinov2 import dinov2
+from keras import Model
+
 app = FastAPI()
+
+model_dinov2: Model | None = None
+model_XGBoost: Model | None = None
 
 # Allowing all middleware is optional, but good practice for dev purposes
 app.add_middleware(
@@ -25,18 +34,47 @@ def predict_tab(
     habitat: str,
     season: str
 ):
-    pass
-    # return {
-    #     "fare": float(prediction)
-    # }
+    global model_XGBoost
+
+    data = pd.DataFrame([
+        cap_shape,
+        cap_color,
+        does_bruise_or_bleed,
+        gill_attachment,
+        gill_color,
+        stem_color,
+        has_ring,
+        habitat,
+        season
+    ])
+
+    if not model_XGBoost:
+        # TODO
+        model_XGBoost = XGBoost.model.load("model/tabular/checkpoints/XGBoost.keras")
+    
+    result = XGBoost.model.predict(model_XGBoost, data)
+
+    return {
+        "result": result
+    }
 
 @app.post("/predict_img")
 def predict_img(
     model: str,
-    image: UploadFile,
+    file: UploadFile,
 ):
-    
-    pass
-    # return {
-    #     "fare": float(prediction)
-    # }
+    global model_dinov2
+
+    image: Image.ImageFile.ImageFile = Image.open(file.file)
+    match model:
+        case "dinov2_baseline":
+            if not model_dinov2:
+                model_dinov2 = dinov2.load_model_from_checkpoint("models/images/dinov2/checkpoints/dinov2.keras")
+            
+            predicted_class, confidence = dinov2.predict(model_dinov2, image)
+        # case ""
+
+    return {
+        "predicted_class": predicted_class,
+        "confidence": confidence
+    }
